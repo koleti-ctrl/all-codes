e3e0b6b3eb8795360f6982689ee141cb35281a2f
koleti-ctrl <koleti@gitam.in>
2025-09-22 16:19:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,8 @@ section[data-testid="stSidebar"] {
 CSV_FILE = "internships.csv"
 try:
     df = pd.read_csv(CSV_FILE)
+st.write(df.columns.tolist())  # DEBUG: see actual column names
+    df.columns = df.columns.str.strip()
 except FileNotFoundError:
     st.error("⚠ Default CSV not found! Put 'internships.csv' in the app folder.")
     st.stop()
@@ -234,3 +236,4 @@ if st.sidebar.button(translate_ui("🔍 Recommend Internships", language), key="
                     if st.button(f"✅ Apply", key=button_key):
                         st.success(f"You chose to apply for {company_name} 🎉")
                         st.markdown(f"[Click here to Apply]({application_link})", unsafe_allow_html=True)
+
